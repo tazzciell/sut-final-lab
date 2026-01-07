0ecea531f80ca9fe6c851f464cc6bb68b2e8a04d
tazzciell <tazzciell@gmail.com>
2026-01-07 15:01:33 +0700

    update negative employeecaode - close #3

---
+++ test/b6629069_negative_employee_code_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	. "github.com/onsi/gomega"
+	"test/entity"
+	"testing"
+)
+
+func TestTask2(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	employee := entity.Employees{
+		Name:         "Tan",
+		Salary:       12000,
+		EmployeeCode: "HR-10245656",
+	}
+
+	ok, err := employee.Validate()
+
+	g.Expect(ok).To(BeFalse())
+	g.Expect(err.Error()).To(ContainSubstring("EmployeeCode must be 2 uppercase English letters (A-Z) followed by ‘-’ and 4 digits (0-9)"))
+}
